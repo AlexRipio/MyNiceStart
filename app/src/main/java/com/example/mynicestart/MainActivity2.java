10627d7ad41311d75a577b970bf66ae8424a2688
Alex <alejandroripio@gmail.com>
2020-12-09 14:50:09 +0100

    1

--- app/src/main/java/com/example/mynicestart/MainActivity2.java
+++ app/src/main/java/com/example/mynicestart/MainActivity2.java
@@ -3,15 +3,12 @@ package com.example.mynicestart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-/**
- * @autor Alejandro on 29/11/20.
- * @version 0.1
- */
-public class LoginActivity extends AppCompatActivity {
+
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login1);
+        setContentView(R.layout.activity_main2);
     }
 }
